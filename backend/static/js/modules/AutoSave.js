4b9b8973844766ef5738810f82f0b2953fc916ed
Mads Erik Forberg <mads@devel.apps8.hardware.no.webdealhosting.com>
2009-04-23 16:25:10 +0200

    added basic static / js dir structure, also began on autosave script

---
+++ backend/static/js/modules/AutoSave.js
@@ -0,0 +1,45 @@
+dojo.provide("modules.AutoSave");
+
+dojo.declare("modules.AutoSave", null, {
+    context: dojo.doc,
+    fields: {
+        int: [],
+        bool: [],
+        string: [],
+    },
+    constructor: function() {
+    },
+    findNodes: function() {
+        dojo.query("form.autosaveForm")
+            .forEach(dojo.hitch(this, function(form) {
+                dojo.query(".autosave", form)
+                    .forEach(dojo.hitch(this, function(elem) {
+                        for (var field in this.fields) {
+                            if (dojo.hasClass(elem, field)) {
+                                this.fields[field].push(elem);
+                            }
+                        }
+                    }));
+        }));
+    },
+    attachEvents: function() {
+        for (var field in this.fields) {
+            dojo.forEach(this.fields[field], dojo.hitch(this, function(node) {
+                console.log(node);
+                dojo.connect(node, "onblur", dojo.hitch(this, "save"));
+            }));
+        }
+    },
+    save: function(evt) {
+        var tNode = evt.target,
+        form = tNode.parentNode;
+
+        while (form.tagName != "FORM") {
+            form = form.parentNode;
+        }
+        dojo.xhrPost({
+            form: form,
+            url: dojo.attr(form, "action")
+        });
+    },
+});
