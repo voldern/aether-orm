9c3348365bc56b8e9765c7668594d2076d56b25c
Mads Erik Forberg <mads@devel.apps8.hardware.no.webdealhosting.com>
2009-04-30 10:41:59 +0200

    fixed removing of onblur event when changes are saved

--- backend/static/js/modules/AutoSave.js
+++ backend/static/js/modules/AutoSave.js
@@ -9,6 +9,7 @@ dojo.declare("modules.AutoSave", null, {
     },
     constructor: function() {
     },
+    ev: null,
     findNodes: function() {
         dojo.query("form.autosaveForm")
             .forEach(dojo.hitch(this, function(form) {
@@ -26,18 +27,17 @@ dojo.declare("modules.AutoSave", null, {
         for (var field in this.fields) {
             dojo.forEach(this.fields[field], dojo.hitch(this, function(node) {
                 dojo.connect(node, "onchange", dojo.hitch(this, function() {
-                    dojo.connect(node, "onblur", dojo.hitch(this, "save"));
+                    this.ev = dojo.connect(node, "onblur", dojo.hitch(this, "save"));
                 }));
             }));
         }
     },
     save: function(evt) {
         var tNode = evt.target,
-        form = tNode.parentNode;
-
-        while (form.tagName != "FORM") {
+        form = tNode.parentNode,
+        load = dojo.place(dojo.create("img", { src: "http://static.tek.no/images/main/spinner.gif" }), tNode, "after");
+        while (form.tagName != "FORM")
             form = form.parentNode;
-        }
 
         dojo.xhr(dojo.attr(form, "method"), {
             handleAs: "json",
@@ -45,7 +45,9 @@ dojo.declare("modules.AutoSave", null, {
             handle: function(response) {
                 console.log(form);
                 console.log(response);
+                dojo.destroy(load);
             }
         }, (dojo.attr(form, "method") == "post") ? true : false);
+        dojo.disconnect(this.ev);
     },
 });
