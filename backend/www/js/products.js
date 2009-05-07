3caa55c99b28c9144ed67838766603d9970fb8ce
Espen Volden <espenv@hardware.no>
2009-05-07 12:53:11 +0200

    Fixed indentation in products.js

--- backend/www/js/products.js
+++ backend/www/js/products.js
@@ -1,6 +1,6 @@
 dojo.require("modules.AutoSave");
 dojo.addOnLoad(function() {
- var auto = new modules.AutoSave;
- auto.findNodes();
- auto.attachEvents();
+    var auto = new modules.AutoSave;
+    auto.findNodes();
+    auto.attachEvents();
 });
