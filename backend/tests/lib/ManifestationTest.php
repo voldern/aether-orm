4188a77fa014726bdd6a9f2216cf7f67d0cf2eba
Raymond Julin <raymond@devel.apps8.hardware.no.webdealhosting.com>
2009-05-07 17:53:20 +0200

    Added save test for manifestation

--- backend/tests/lib/ManifestationTest.php
+++ backend/tests/lib/ManifestationTest.php
@@ -28,4 +28,27 @@ class ManifestationTest extends PHPUnit_Framework_TestCase {
         $db->query("DELETE FROM manifestation WHERE entity_id = $id");
         $db->query("DELETE FROM work WHERE entity_id = $workId");
     }
+
+    public function testSaveManifestation() {
+        $db = new Database('pg2_backend');
+        $title = 'GeForce 6600 512 MB';
+        $work = Work::create('GeForce 6600');
+        $manifestation = Manifestation::create($work, $title);
+        $id = $manifestation->get('id');
+        $workId = $work->get('id');
+
+
+        $t2 = 'Hei';
+        $manifestation->set('title', $t2);
+        $manifestation->save();
+
+        $entity = $db->queryRowf(
+            'SELECT * FROM manifestation_view WHERE id = ?',$id);
+        $this->assertEquals($t2, $entity['title']);
+        // Delete entry
+        $db->query("DELETE FROM entity WHERE id = $id");
+        $db->query("DELETE FROM entity WHERE id = $workId");
+        $db->query("DELETE FROM manifestation WHERE entity_id = $id");
+        $db->query("DELETE FROM work WHERE entity_id = $workId");
+    }
 }
