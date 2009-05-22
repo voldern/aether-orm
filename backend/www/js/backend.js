d9cd10f9063d9b2b45fb6beeeb67ae85d2402ff1
Mads Erik Forberg <mads@devel.apps8.hardware.no.webdealhosting.com>
2009-05-22 11:43:58 +0200

    ../css/style.css
    added js for appending close button to .notice elems, also som styling for icons

--- backend/www/js/backend.js
+++ backend/www/js/backend.js
@@ -1,1 +1,18 @@
+// Register custom module path
 dojo.registerModulePath("modules", "../../modules");
+
+// Requires
+dojo.require("dojo.behavior");
+
+dojo.addOnLoad(function() {
+    dojo.behavior.add({
+        ".notice": function(n) {
+            dojo.query("<a class=\"icon close\" href=\"#\">Close</a>")
+                .connect("onclick", function(evt) {
+                    dojo.stopEvent(evt);
+                    dojo.destroy(this.parentNode);
+                }).appendTo(n);
+        }
+    });
+    dojo.behavior.apply();
+});
