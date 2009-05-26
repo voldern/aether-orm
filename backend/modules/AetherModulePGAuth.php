fc717a607b9675a022ee030ac28bb70d80a1bb3a
Espen Volden <espenv@hardware.no>
2009-05-26 14:03:59 +0200

    Made it possible to require that a used should be logged out on a certain page

--- backend/modules/AetherModulePGAuth.php
+++ backend/modules/AetherModulePGAuth.php
@@ -19,8 +19,21 @@ class AetherModulePGAuth extends AetherModule {
         $tpl = $this->sl->getTemplate();
         $config = $this->sl->get('aetherConfig');
         $options = $config->getOptions();
-        $loginRequired = $options['loginRequired'];
 
+        // Always assume that the user needs to be logged in by default
+        if (isset($options['loginRequired']) &&
+            in_array($options['loginRequired'], array('true', 'false')))
+            $loginRequired = $options['loginRequired'];
+        else
+            $loginRequired = 'true';
+
+        // Always assume that the user does _not_ need to be logged out by default
+        if (isset($options['logoutRequired']) &&
+            in_array($options['logoutRequired'], array('true', 'false')))
+            $logoutRequired = $options['logoutRequired'];
+        else
+            $logoutRequired = 'false';
+        
         // Check if the user is logged in
         if (isset($_SESSION['authInfo']) &&
             isset($_SESSION['authInfo']['verified']) &&
@@ -30,6 +43,9 @@ class AetherModulePGAuth extends AetherModule {
         else
             $loggedIn = false;
 
+        if ($loginRequired === 'true' && $logoutRequired === 'true')
+            throw new Exception('Login and logout cannot both be required at the same time');
+        
         // If the user is required to be logged in but is not
         // redirect to the login page
         if ($loginRequired === 'true' && $loggedIn === false) {
@@ -37,9 +53,17 @@ class AetherModulePGAuth extends AetherModule {
             header('Location: /login?error=login_required&referer=' . $referer);
             die();
         }
-        else
-            $tpl->set('loggedIn', $loggedIn);
+        elseif ($logoutRequired === 'true' && $loggedIn === true) {
+            // If the user has to be logged out to access the page redirect him
+            // where he came from
+            if (isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER']))
+                header('Location: ' . $_SERVER['HTTP_REFERER']);
+            else
+                header('Location: /');
+            die();
+        }
+        
+        $tpl->set('loggedIn', $loggedIn);
     }
-
 }
 ?>
