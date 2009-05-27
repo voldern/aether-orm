c67be8c1e7cbdc725f6d846dbc99179dfb14c7d3
Simen Graaten <simeng@devel.apps8.hardware.no.webdealhosting.com>
2009-05-27 19:09:51 +0200

    Added an example config for the new general autoloader

---
+++ backend/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * Example projectconfig for backend
+ * 
+ * Created: 2009-05-27
+ * @author Simen Graaten
+ * @package pg2.backend
+ */
+
+$_autoload_path = array(
+    "Priceguide" => array("/home/simeng/lib/priceguide"),
+    "AetherModule" => array("/home/simeng/public_html/pg2/backend/modules"),
+    "" => array("/home/simeng/lib", "/home/simeng/lib/article/new2"),
+);
+
+?>
