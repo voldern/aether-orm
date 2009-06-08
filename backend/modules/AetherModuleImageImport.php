a11fa4233fe1372629af7ffff7bdf3f65125416b
Espen Volden <espenv@hardware.no>
2009-06-08 14:34:39 +0200

    Initial import of AetherModuleImageImport

---
+++ backend/modules/AetherModuleImageImport.php
@@ -0,0 +1,26 @@
+<?php // 
+/**
+ * 
+ * Show a single product for editing
+ * 
+ * Created: 2009-06-08
+ * @author Espen Volden
+ * @package prisguide.backend.modules
+ */
+
+class AetherModuleImageImport extends AetherModule {
+    /**
+     * Render module
+     *
+     * @access public
+     * @return string
+     */
+    public function run() {
+        $tpl = $this->sl->getTemplate();
+        $config = $this->sl->get('aetherConfig');
+        $pid = $config->getUrlVar('product_id');
+        $tpl->set('id', $pid);
+        
+        return $tpl->fetch('image/import.tpl');
+    }
+}
