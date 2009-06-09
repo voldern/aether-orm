b4d7338d04b24bd864b2b67170d5302d346de5e8
Espen Volden <espenv@hardware.no>
2009-06-09 16:18:43 +0200

    Made ImageImport.js run an ajax call getting images associated with a set of products and/or articles

--- backend/www/js/modules/ImageImport.js
+++ backend/www/js/modules/ImageImport.js
@@ -1,6 +1,6 @@
 /**
  * Make it possible to search for a image from old systems and import it
- */
+  */
 
 dojo.provide('modules.ImageImport');
 dojo.require('dijit._Widget');
@@ -10,4 +10,28 @@ dojo.declare('modules.ImageImport',
              [dijit._Widget, dojox.dtl._DomTemplated], {
                  // Use a file based html template
                  templatePath: dojo.moduleUrl('modules.templates', 'image_import.html'),
+                 postCreate: function() {
+                     dojo.query('#image_import_submit').connect(
+                         'onclick', dojo.hitch(this, function(e) {
+                             dojo.stopEvent(e);
+                             var articles = dojo.byId('articles_input').value;
+                             var products = dojo.byId('products_input').value;
+
+                             dojo.xhrGet({
+                                 url: '',
+                                 content: {
+                                     module: 'ImageImport',
+                                     service: 'lookIn',
+                                     articles: articles,
+                                     products: products,
+                                 },
+                                 handleAs: 'json',
+                                 load: dojo.hitch(this, function(resp, ioArgs) {
+                                     console.log(resp);
+                                     console.log(ioArgs);
+                                 }),
+                             });
+                         })
+                     );
+                 },
              });
