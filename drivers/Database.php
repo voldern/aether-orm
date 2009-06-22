cfd21ba3810e05c926e281a223f9d473126a91b2
Espen Volden <voldern@arche.hoeggen.net>
2009-06-22 17:01:14 +0200

    Finished porting Kohana code to our standars and removing Kohana specific things like logging and config

--- drivers/Database.php
+++ drivers/Database.php
@@ -1,1 +1,566 @@
 <?php
+
+abstract class DatabaseDriver {
+    protected $queryCache;
+
+    abstract public function connect();
+
+    abstract public function query($sql);
+	/**
+	 * Builds a DELETE query.
+	 *
+	 * @param   string  table name
+	 * @param   array   where clause
+	 * @return  string
+	 */
+	public function delete($table, $where)
+	{
+		return 'DELETE FROM '.$this->escape_table($table).' WHERE '.implode(' ', $where);
+	}
+
+	/**
+	 * Builds an UPDATE query.
+	 *
+	 * @param string $table table name
+	 * @param array $values key => value pairs
+	 * @param array $where where clause
+	 * @return string
+	 */
+	public function update($table, $values, $where) {
+		foreach ($values as $key => $val) {
+			$valstr[] = $this->escapeColumn($key).' = '.$val;
+		}
+        
+		return 'UPDATE ' . $this->escapeTable($table) . ' SET ' .
+            implode(', ', $valstr) . ' WHERE ' . implode(' ',$where);
+	}
+
+	/**
+	 * Set the charset using 'SET NAMES <charset>'.
+	 *
+	 * @param string $charset character set to use
+	 */
+	public function setCharset($charset) {
+		throw new DatabaseException('not_implemented ' . __FUNCTION__);
+	}
+
+	/**
+	 * Wrap the tablename in backticks, has support for: table.field syntax.
+	 *
+	 * @param string $table table name
+	 * @return string
+	 */
+	abstract public function escapeTable($table);
+
+	/**
+	 * Escape a column/field name, has support for special commands.
+	 *
+	 * @param string $column column name
+	 * @return string
+	 */
+	abstract public function escapeColumn($column);
+
+	/**
+	 * Builds a WHERE portion of a query.
+	 *
+	 * @param mixed $key key
+	 * @param string $value value
+	 * @param string $type type
+	 * @param int $numWheres number of where clauses
+	 * @param boolean $quote escape the value
+	 * @return string
+	 */
+	public function where($key, $value, $type, $numWheres, $quote) {
+		$prefix = ($numWheres == 0) ? '' : $type;
+
+		if ($quote === -1) {
+			$value = '';
+		}
+		else {
+			if ($value === null) {
+				if (!$this->hasOperator($key))
+					$key .= ' IS';
+
+				$value = ' NULL';
+			}
+			elseif (is_bool($value)) {
+				if (!$this->hasOperator($key)) {
+					$key .= ' =';
+				}
+
+				$value = ($value == true) ? ' 1' : ' 0';
+			}
+			else {
+				if (!$this->hasOperator($key) && !empty($key)) {
+					$key = $this->escapeColumn($key) . ' =';
+				}
+				else {
+					preg_match('/^(.+?)([<>!=]+|\bIS(?:\s+NULL))\s*$/i', $key, $matches);
+					if (isset($matches[1]) && isset($matches[2])) {
+						$key = $this->escapeColumn(trim($matches[1])) . ' ' .
+                            trim($matches[2]);
+					}
+				}
+
+				$value = ' '. (($quote == true) ? $this->escape($value) : $value);
+			}
+		}
+
+		return $prefix . $key . $value;
+	}
+
+	/**
+	 * Builds a LIKE portion of a query.
+	 *
+	 * @param mixed $field field name
+	 * @param string $match value to match with field
+	 * @param boolean $auto add wildcards before and after the match
+	 * @param string $type clause type (AND or OR)
+	 * @param int $numLikes number of likes
+	 * @return string
+	 */
+	public function like($field, $match, $auto, $type, $numLikes) {
+		$prefix = ($numLikes == 0) ? '' : $type;
+
+		$match = $this->escapeStr($match);
+
+		if ($auto === true) {
+			// Add the start and end quotes
+			$match = '%'.str_replace('%', '\\%', $match).'%';
+		}
+
+		return $prefix . ' ' . $this->escapeColumn($field) . " LIKE '$match'";
+	}
+
+	/**
+	 * Builds a NOT LIKE portion of a query.
+	 *
+	 * @param mixed $field field name
+	 * @param string $match value to match with field
+     * @param boolean $auto add wildcards before and after the match
+	 * @param string $type clause type (AND or OR)
+	 * @param int $numLikes number of likes
+	 * @return string
+	 */
+	public function notlike($field, $match, $auto, $type, $numLikes) {
+		$prefix = ($num_likes == 0) ? '' : $type;
+
+		$match = $this->escapeStr($match);
+
+		if ($auto === true) {
+			// Add the start and end quotes
+			$match = '%'.$match.'%';
+		}
+
+		return $prefix . ' ' . $this->escapeColumn($field) . " NOT LIKE '$match'";
+	}
+
+	/**
+	 * Builds a REGEX portion of a query.
+	 *
+	 * @param string $field field name
+	 * @param string $match value to match with field
+	 * @param string $type clause type (AND or OR)
+	 * @param integer $numRegexs number of regexes
+	 * @return string
+	 */
+	public function regex($field, $match, $type, $numRegexs) {
+		throw new DatabaseException('not_implemented ' . __FUNCTION__);
+	}
+
+	/**
+	 * Builds a NOT REGEX portion of a query.
+	 *
+	 * @param string $field field name
+	 * @param string $match value to match with field
+	 * @param string $type clause type (AND or OR)
+	 * @param integer $numRegexs number of regexes
+	 * @return string
+	 */
+	public function notregex($field, $match, $type, $numRegexs) {
+		throw new DatabaseException('not_implemented ' . __FUNCTION__);
+	}
+
+	/**
+	 * Builds an INSERT query.
+	 *
+	 * @param string $table table name
+	 * @param array $keys keys
+	 * @param array $values  values
+	 * @return string
+	 */
+	public function insert($table, $keys, $values) {
+		// Escape the column names
+		foreach ($keys as $key => $value) {
+			$keys[$key] = $this->escapeColumn($value);
+		}
+        
+		return 'INSERT INTO ' . $this->escapeTable($table) . ' (' .
+            implode(', ', $keys) . ') VALUES (' . implode(', ', $values) . ')';
+	}
+
+	/**
+	 * Builds a MERGE portion of a query.
+	 *
+	 * @param string $table table name
+	 * @param array $keys keys
+	 * @param array $values values
+	 * @return string
+	 */
+	public function merge($table, $keys, $values) {
+		throw new DatabaseException('not_implemented ' . __FUNCTION__);
+	}
+
+	/**
+	 * Builds a LIMIT portion of a query.
+	 *
+	 * @param integer $limit limit
+	 * @param integer $offset defaults to 0
+	 * @return string
+	 */
+	abstract public function limit($limit, $offset = 0);
+
+	/**
+	 * Creates a prepared statement.
+	 *
+	 * @param   string  SQL query
+	 * @return  DatabaseStmt
+	 */
+	public function stmtPrepare($sql = '') {
+		throw new DatabaseException('not_implemented ' . __FUNCTION__);
+	}
+
+	/**
+	 *  Compiles the SELECT statement.
+	 *  Generates a query string based on which functions were used.
+	 *  Should not be called directly, the get() function calls it.
+	 *
+	 * @param array $database select query values
+	 * @return string
+	 */
+	abstract public function compileSelect($database);
+
+	/**
+	 * Determines if the string has an arithmetic operator in it.
+	 *
+	 * @param string $str string to check
+	 * @return boolean
+	 */
+	public function hasOperator($str) {
+		return (bool)preg_match('/[<>!=]|\sIS(?:\s+NOT\s+)?\b|BETWEEN/i', trim($str));
+	}
+
+	/**
+	 * Escapes any input value.
+	 *
+	 * @param mixed $value value to escape
+	 * @return string
+	 */
+	public function escape($value) {
+		if (!$this->dbConfig['escape'])
+			return $value;
+
+		switch (gettype($value)) {
+			case 'string':
+				$value = '\'' . $this->escapeStr($value) . '\'';
+                break;
+			case 'boolean':
+				$value = (int)$value;
+                break;
+			case 'double':
+				// Convert to non-locale aware float to prevent possible commas
+				$value = sprintf('%F', $value);
+                break;
+			default:
+				$value = ($value === NULL) ? 'NULL' : $value;
+                break;
+		}
+
+		return (string)$value;
+	}
+
+	/**
+	 * Escapes a string for a query.
+	 *
+	 * @param mixed $str value to escape
+	 * @return string
+	 */
+	abstract public function escapeStr($str);
+
+	/**
+	 * Lists all tables in the database.
+	 *
+	 * @return array
+	 */
+	abstract public function listTables();
+
+	/**
+	 * Lists all fields in a table.
+	 *
+	 * @param string $table table name
+	 * @return array
+	 */
+	abstract function listFields($table);
+
+	/**
+	 * Returns the last database error.
+	 *
+	 * @return string
+	 */
+	abstract public function showError();
+
+	/**
+	 * Returns field data about a table.
+	 *
+	 * @param string $table table name
+	 * @return array
+	 */
+	abstract public function fieldData($table);
+
+	/**
+	 * Fetches SQL type information about a field, in a generic format.
+	 *
+	 * @param string $str field datatype
+	 * @return array
+	 */
+	protected function sqlType($str) {
+		static $sqlTypes;
+
+		if ($sqlTypes === NULL) {
+			// Load SQL data types
+			$sqlTypes = Config::retrieve('sql_types');
+		}
+
+		$str = strtolower(trim($str));
+
+		if (($open  = strpos($str, '(')) !== false) {
+			// Find closing bracket
+			$close = strpos($str, ')', $open) - 1;
+
+			// Find the type without the size
+			$type = substr($str, 0, $open);
+		}
+		else {
+			// No length
+			$type = $str;
+		}
+
+        if (empty($sqlTypes[$type])) {
+            exit('Unknown field type: ' . $type . '. ' .
+                 'Please report this: drift@hardware.no!');
+        }
+
+		// Fetch the field definition
+		$field = $sqlTypes[$type];
+
+		switch ($field['type'])
+		{
+			case 'string':
+			case 'float':
+				if (isset($close)) {
+					// Add the length to the field info
+					$field['length'] = substr($str, $open + 1, $close - $open);
+				}
+			break;
+			case 'int':
+				// Add unsigned value
+				$field['unsigned'] = (strpos($str, 'unsigned') !== false);
+			break;
+		}
+
+		return $field;
+	}
+
+	/**
+	 * Clears the internal query cache.
+	 *
+	 * @param string $sql SQL query
+	 */
+	public function clearCache($sql = NULL) {
+		if (empty($sql)) {
+			$this->queryCache = array();
+		}
+		else {
+			unset($this->queryCache[$this->queryHash($sql)]);
+		}
+
+        // ::log('debug', 'Database cache cleared: '.get_class($this));
+	}
+
+	/**
+	 * Creates a hash for an SQL query string. Replaces newlines with spaces,
+	 * trims, and hashes.
+	 *
+	 * @param string $sql SQL query
+	 * @return string
+	 */
+	protected function query_hash($sql) {
+		return sha1(str_replace("\n", ' ', trim($sql)));
+	}
+}
+
+abstract class DatabaseResult implements ArrayAccess, Iterator, Countable {
+
+	// Result resource, insert id, and SQL
+	protected $result;
+	protected $insertId;
+	protected $sql;
+
+	// Current and total rows
+	protected $currentRow = 0;
+	protected $totalRows  = 0;
+
+	// Fetch function and return type
+	protected $fetchType;
+	protected $returnType;
+
+	/**
+	 * Returns the SQL used to fetch the result.
+	 *
+	 * @return string
+	 */
+	public function sql() {
+		return $this->sql;
+	}
+
+	/**
+	 * Returns the insert id from the result.
+	 *
+	 * @return  mixed
+	 */
+	public function insertId() {
+		return $this->insert_id;
+	}
+
+	/**
+	 * Prepares the query result.
+	 *
+	 * @param boolean $object return rows as objects
+	 * @param mixed $type type
+	 * @return DatabaseResult
+	 */
+	abstract function result($object = TRUE, $type = FALSE);
+
+	/**
+	 * Builds an array of query results.
+	 *
+	 * @param boolean $object return rows as objects
+	 * @param mixed $type type
+	 * @return array
+	 */
+	abstract function resultArray($object = NULL, $type = FALSE);
+
+	/**
+	 * Gets the fields of an already run query.
+	 *
+	 * @return array
+	 */
+	abstract public function listFields();
+
+	/**
+	 * Seek to an offset in the results.
+	 *
+     * @param int $offset
+	 * @return  boolean
+	 */
+	abstract public function seek($offset);
+
+	/**
+	 * Countable: count
+	 */
+	public function count() {
+		return $this->totalRows;
+	}
+
+	/**
+	 * ArrayAccess: offsetExists
+	 */
+	public function offsetExists($offset) {
+		if ($this->totalRows > 0) {
+			$min = 0;
+			$max = $this->totalRows - 1;
+
+            if ($offset < $min || $offset > $max)
+                return false;
+            else
+                return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * ArrayAccess: offsetGet
+	 */
+	public function offsetGet($offset) {
+		if (!$this->seek($offset))
+			return false;
+
+		// Return the row by calling the defined fetching callback
+		return call_user_func($this->fetch_type, $this->result, $this->return_type);
+	}
+
+	/**
+	 * ArrayAccess: offsetSet
+	 *
+	 * @throws  Kohana_Database_Exception
+	 */
+	final public function offsetSet($offset, $value) {
+		throw new DatabaseException('result_read_only');
+	}
+
+	/**
+	 * ArrayAccess: offsetUnset
+	 *
+	 * @throws  Kohana_Database_Exception
+	 */
+	final public function offsetUnset($offset) {
+		throw new DatabaseException('result_read_only');
+	}
+
+	/**
+	 * Iterator: current
+	 */
+	public function current() {
+		return $this->offsetGet($this->currentRow);
+	}
+
+	/**
+	 * Iterator: key
+	 */
+	public function key() {
+		return $this->currentRow;
+	}
+
+	/**
+	 * Iterator: next
+	 */
+	public function next() {
+		++$this->currentRow;
+		return $this;
+	}
+
+	/**
+	 * Iterator: prev
+	 */
+	public function prev() {
+		--$this->currentRow;
+		return $this;
+	}
+
+	/**
+	 * Iterator: rewind
+	 */
+	public function rewind() {
+		$this->currentRow = 0;
+		return $this;
+	}
+
+	/**
+	 * Iterator: valid
+	 */
+	public function valid() {
+		return $this->offsetExists($this->currentRow);
+	}
+
+}
--- drivers/Database/Pgsql.php
+++ drivers/Database/Pgsql.php
@@ -1,1 +1,431 @@
 <?php
+
+class DatabasePgsqlDriver extends DatabaseDriver {
+    // Database connection link
+    protected $link;
+    protected $dbConfig;
+
+    /**
+     * Sets the config for the class
+     *
+     * @param array $config database configuration
+     */
+    public function __construct($config) {
+        $this->dbConfig = $config;
+
+        // ::log('debug', 'PgSQL database driver initialized');
+    }
+
+    public function connect() {
+        // Check if link already exists
+        if (is_resource($this->link))
+            return $this->link;
+
+        // Import conenction variables
+        extract($this->dbConfig['connection']);
+
+        // Persisten connections enabled?
+        $connect = ($this->dbConfig['persistent'] === true) ?
+            'pg_pconnect' : 'pg_connect';
+        
+        // Build the connection info
+		$port = isset($port) ? 'port=\''.$port.'\'' : '';
+        // if no host, connect with the socket
+		$host = isset($host) ? 'host=\''.$host.'\' '.$port : '';
+
+        $connectionString = "$host dbname='$database' user='$user' password='$pass'";
+
+        // Make the connection and select the database
+        if ($this->link = $connect($connectionString)) {
+            if ($charset = $this->dbConfig['character_set'])
+                echo $this->setCharset($charset);
+
+            // Clear password after successful connect
+            $this->dbConfig['connection']['pass'] = null;
+
+            return $this->link;
+        }
+
+        return false;
+    }
+
+    public function query($sql) {
+        // Only cache if it's turned on, and only cache if it's not a write statement
+        if ($this->dbConfig['cache'] &&
+            !preg_match('#\b(?:INSERT|UPDATE|SET)\b#i', $sql)) {
+            $hash = $this->queryHash($sql);
+
+            if (!isset($this->queryCache[$hash])) {
+                // Set the cached object
+                $this->queryCache[$hash] =
+                    new PgsqlResult(pg_query($this->link, $sql), $this->link,
+                                    $this->dbConfig['object'], $sql);
+            }
+            else {
+                // Rewind cached result
+                $this->queryCache[$hash]->rewind();
+            }
+
+            return $this->queryCache[$hash];
+        }
+
+        // Suppress warning triggered when a database error occurs (e.g., a constraint violation)
+        return new PgsqlResult(@pg_query($this->link, $sql), $this->link,
+                               $this->dbConfig['object'], $sql);
+    }
+
+    public function setCharset($charset) {
+        $this->query('SET client_encoding TO ' . pg_escape_string($this->link, $charset));
+    }
+
+    public function escapeTable($table) {
+        if (!$this->dbConfig['escape'])
+            return $table;
+
+        return '"'.str_replace('.', '"."', $table).'"';
+    }
+
+    public function escapeColumn($column) {
+        if (!$this->dbConfig['escape'])
+            return $column;
+
+        if ($column == '*')
+            return $column;
+
+        // This matches any functions we support to SELECT.
+		if (preg_match('/(avg|count|sum|max|min)\(\s*(.*)\s*\)(\s*as\s*(.+)?)?/i',
+                       $column, $matches)) {
+            
+			if (count($matches) == 3) {
+				return $matches[1] . '(' . $this->escapeColumn($matches[2]) . ')';
+			}
+			elseif (count($matches) == 5) {
+				return $matches[1]. '(' . $this->escapeColumn($matches[2]) .
+                    ') AS ' .$this->escapeColumn($matches[2]);
+			}
+		}
+
+		// This matches any modifiers we support to SELECT.
+		if (!preg_match('/\b(?:all|distinct)\s/i', $column)) {
+			if (stripos($column, ' AS ') !== false) {
+				// Force 'AS' to uppercase
+				$column = str_ireplace(' AS ', ' AS ', $column);
+
+				// Runs escapeColumn on both sides of an AS statement
+				$column = array_map(array($this, __FUNCTION__), explode(' AS ', $column));
+
+				// Re-create the AS statement
+				return implode(' AS ', $column);
+			}
+
+			return preg_replace('/[^.*]+/', '"$0"', $column);
+		}
+
+		$parts = explode(' ', $column);
+		$column = '';
+
+		for ($i = 0, $c = count($parts); $i < $c; $i++) {
+			// The column is always last
+			if ($i == ($c - 1)) {
+				$column .= preg_replace('/[^.*]+/', '"$0"', $parts[$i]);
+			}
+            // otherwise, it's a modifier
+			else {
+				$column .= $parts[$i] . ' ';
+			}
+		}
+        
+		return $column;
+    }
+
+	public function regex($field, $match, $type, $numRegexs) {
+		$prefix = ($numRegexs == 0) ? '' : $type;
+
+		return $prefix . ' ' . $this->escapeColumn($field) .
+            ' ~* \'' . $this->escapeStr($match) . '\'';
+	}
+
+    public function notregex($field, $match, $type, $numRegexs) {
+		$prefix = $numRegexs == 0 ? '' : $type;
+
+		return $prefix . ' ' . $this->escapeColumn($field) .
+            ' !~* \'' . $this->escapeStr($match) . '\'';
+	}
+
+	public function limit($limit, $offset = 0) {
+		return 'LIMIT ' . $limit . ' OFFSET ' . $offset;
+	}
+
+    public function compileSelect($database) {
+		$sql = ($database['distinct'] == true) ? 'SELECT DISTINCT ' : 'SELECT ';
+		$sql .= (count($database['select']) > 0) ? implode(', ', $database['select']) : '*';
+
+		if (count($database['from']) > 0) {
+			$sql .= "\nFROM ";
+			$sql .= implode(', ', $database['from']);
+		}
+
+		if (count($database['join']) > 0) {
+			foreach($database['join'] AS $join) {
+				$sql .= "\n" . $join['type'] . 'JOIN ' .
+                    implode(', ', $join['tables']) . ' ON ' .
+                    $join['conditions'];
+			}
+		}
+
+		if (count($database['where']) > 0) {
+			$sql .= "\nWHERE ";
+            $sql .= implode("\n", $database['where']);
+        }
+
+		if (count($database['groupby']) > 0) {
+			$sql .= "\nGROUP BY ";
+			$sql .= implode(', ', $database['groupby']);
+		}
+
+		if (count($database['having']) > 0) {
+			$sql .= "\nHAVING ";
+			$sql .= implode("\n", $database['having']);
+		}
+
+		if (count($database['orderby']) > 0) {
+			$sql .= "\nORDER BY ";
+			$sql .= implode(', ', $database['orderby']);
+		}
+
+		if (is_numeric($database['limit'])) {
+			$sql .= "\n";
+			$sql .= $this->limit($database['limit'], $database['offset']);
+		}
+
+		return $sql;
+	}
+
+    public function escapeStr($str)
+	{
+		if (!$this->dbConfig['escape'])
+			return $str;
+
+        if (!is_resource($this->link))
+            $this->connect();
+
+		return pg_escape_string($this->link, $str);
+	}
+    
+    public function listTables() {
+		$sql = "SELECT table_schema || '.' || table_name
+                FROM information_schema.tables WHERE
+                table_schema NOT IN ('pg_catalog', 'information_schema')";
+		$result = $this->query($sql)->result(FALSE, PGSQL_ASSOC);
+
+		$retval = array();
+		foreach ($result as $row)
+			$retval[] = current($row);
+
+		return $retval;
+	}
+
+    public function showError() {
+		return pg_last_error($this->link);
+	}
+
+    public function listFields($table)
+	{
+		$result = NULL;
+
+		foreach ($this->fieldData($table) as $row) {
+			// Make an associative array
+			$result[$row->column_name] = $this->sqlType($row->data_type);
+
+			if (!strncmp($row->column_default, 'nextval(', 8))
+				$result[$row->column_name]['sequenced'] = TRUE;
+
+			if ($row->is_nullable === 'YES')
+				$result[$row->column_name]['null'] = TRUE;
+		}
+
+		if (!isset($result))
+			throw new DatabaseException('table_not_found ' . $table);
+
+		return $result;
+	}
+
+    public function fieldData($table)
+	{
+		// http://www.postgresql.org/docs/8.3/static/infoschema-columns.html
+		$result = $this->query('
+			SELECT column_name, column_default, is_nullable, data_type, udt_name,
+				character_maximum_length, numeric_precision, numeric_precision_radix, numeric_scale
+			FROM information_schema.columns
+			WHERE table_name = \''. $this->escapeStr($table) .'\'
+			ORDER BY ordinal_position
+		');
+
+		return $result->resultArray(TRUE);
+	}
+}
+
+class PgsqlResult extends DatabaseResult {
+    // Data fetching types
+	protected $fetchType  = 'pgsql_fetch_object';
+	protected $returnType = PGSQL_ASSOC;
+
+    /**
+	 * Sets up the result variables.
+	 *
+	 * @param resource $result query result
+	 * @param resource $link database link
+	 * @param boolean $object return objects or arrays
+	 * @param string $sql SQL query that was run
+	 */
+	public function __construct($result, $link, $object = TRUE, $sql) {
+		$this->link = $link;
+		$this->result = $result;
+
+		// If the query is a resource, it was a SELECT, SHOW, DESCRIBE, EXPLAIN query
+		if (is_resource($result)) {
+			// Its an DELETE, INSERT, REPLACE, or UPDATE query
+			if (preg_match('/^(?:delete|insert|replace|update)\b/iD',
+                           trim($sql), $matches)) {
+				$this->insertId  = (strtolower($matches[0]) == 'insert') ? $this->insertId() : FALSE;
+				$this->totalRows = pg_affected_rows($this->result);
+			}
+			else {
+				$this->currentRow = 0;
+				$this->totalRows  = pg_num_rows($this->result);
+				$this->fetchType = ($object === TRUE) ? 'pg_fetch_object' : 'pg_fetch_array';
+			}
+		}
+		else {
+			throw new DatabaseException('pgsql error: ' . pg_last_error() .
+                                        ' - ' . $sql);
+		}
+
+		// Set result type
+		$this->result($object);
+
+		// Store the SQL
+		$this->sql = $sql;
+	}
+
+	/**
+	 * Magic __destruct function, frees the result.
+	 */
+	public function __destruct() {
+		if (is_resource($this->result))
+			pg_free_result($this->result);
+	}
+
+    public function result($object = true, $type = PGSQL_ASSOC) {
+		$this->fetchType = ((bool)$object) ? 'pg_fetch_object' : 'pg_fetch_array';
+
+		// This check has to be outside the previous statement, because we do not
+		// know the state of fetch_type when $object = NULL
+		// NOTE - The class set by $type must be defined before fetching the result,
+		// autoloading is disabled to save a lot of stupid overhead.
+		if ($this->fetchType == 'pg_fetch_object') {
+			$this->returnType = (is_string($type) && Config::autoLoad($type)) ?
+                $type : 'stdClass';
+		}
+		else {
+			$this->returnType = $type;
+		}
+
+		return $this;
+	}
+
+	public function asArray($object = null, $type = PGSQL_ASSOC) {
+		return $this->resultArray($object, $type);
+	}
+
+	public function resultArray($object = null, $type = PGSQL_ASSOC) {
+		$rows = array();
+
+		if (is_string($object)) {
+			$fetch = $object;
+		}
+		elseif (is_bool($object)) {
+			if ($object === true) {
+				$fetch = 'pg_fetch_object';
+
+				// NOTE - The class set by $type must be defined before fetching the result,
+				// autoloading is disabled to save a lot of stupid overhead.
+				$type = (is_string($type) && Config::autoLoad($type)) ?
+                    $type : 'stdClass';
+			}
+			else {
+				$fetch = 'pg_fetch_array';
+			}
+		}
+		else {
+			// Use the default config values
+			$fetch = $this->fetchType;
+
+			if ($fetch == 'pg_fetch_object') {
+				$type = (is_string($type) && Config::autoLoad($type)) ? $type : 'stdClass';
+			}
+		}
+
+		if ($this->totalRows) {
+			pg_result_seek($this->result, 0);
+
+			while ($row = $fetch($this->result, NULL, $type))
+				$rows[] = $row;
+		}
+
+		return $rows;
+	}
+
+	public function insertId() {
+		if ($this->insertId === NULL) {
+			$query = 'SELECT LASTVAL() AS insert_id';
+
+			// Disable error reporting for this, just to silence errors on
+			// tables that have no serial column.
+			$ER = error_reporting(0);
+
+			$result = pg_query($this->link, $query);
+			$insertId = pg_fetch_array($result, NULL, PGSQL_ASSOC);
+
+			$this->insertId = $insertId['insert_id'];
+
+			// Reset error reporting
+			error_reporting($ER);
+		}
+
+		return $this->insertId;
+	}
+
+	public function seek($offset) {
+		if ($this->offsetExists($offset) && pg_result_seek($this->result, $offset)) {
+			// Set the current row to the offset
+			$this->currentRow = $offset;
+
+			return true;
+		}
+
+		return false;
+	}
+
+	public function listFields() {
+		$fieldNames = array();
+
+		$fields = pg_num_fields($this->result);
+		for ($i = 0; $i < $fields; ++$i)
+			$fieldNames[] = pg_field_name($this->result, $i);
+
+		return $fieldNames;
+	}
+
+	/**
+	 * ArrayAccess: offsetGet
+	 */
+	public function offsetGet($offset) {
+		if (!$this->seek($offset))
+			return false;
+
+		// Return the row by calling the defined fetching callback
+		$fetch = $this->fetchType;
+		return $fetch($this->result, NULL, $this->returnType);
+	}
+}
