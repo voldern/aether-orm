fb117eee658de1641796d59d1ef9170b9f0f2ebf
Espen Volden <voldern@arche.hoeggen.net>
2009-06-23 16:03:05 +0200

    Ported Database_Pdosqlite_Driver to aether standards

--- drivers/Database/Pdosqlite.php
+++ drivers/Database/Pdosqlite.php
@@ -1,118 +1,93 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-/*
- * Class: Database_PdoSqlite_Driver
- *  Provides specific database items for Sqlite.
- *
- * Connection string should be, eg: "pdosqlite://path/to/database.db"
- *
- * Version 1.0 alpha
- *  author    - Doutu, updated by gregmac
- *  copyright - (c) BSD
- *  license   - <no>
- */
+<?php
 
-class Database_Pdosqlite_Driver extends Database_Driver {
+class DatabasePdosqliteDriver extends DatabaseDriver {
 
 	// Database connection link
 	protected $link;
-	protected $db_config;
+	protected $dbConfig;
 
 	/*
 	 * Constructor: __construct
 	 *  Sets up the config for the class.
 	 *
-	 * Parameters:
-	 *  config - database configuration
-	 *
+	 * @param string $config database config
 	 */
-	public function __construct($config)
-	{
-		$this->db_config = $config;
+	public function __construct($config) {
+		$this->dbConfig = $config;
 
-		Kohana::log('debug', 'PDO:Sqlite Database Driver Initialized');
+		// ::log('debug', 'PDO:Sqlite Database Driver Initialized');
 	}
 
-	public function connect()
-	{
+	public function connect() {
 		// Import the connect variables
-		extract($this->db_config['connection']);
+		extract($this->dbConfig['connection']);
 
-		try
-		{
+		try {
 			$this->link = new PDO('sqlite:'.$socket.$database, $user, $pass,
-				array(PDO::ATTR_PERSISTENT => $this->db_config['persistent']));
+				array(PDO::ATTR_PERSISTENT => $this->dbConfig['persistent']));
 
 			$this->link->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
 			//$this->link->query('PRAGMA count_changes=1;');
 
-			if ($charset = $this->db_config['character_set'])
-			{
-				$this->set_charset($charset);
-			}
+			if ($charset = $this->dbConfig['character_set'])
+				$this->setCharset($charset);
 		}
-		catch (PDOException $e)
-		{
-			throw new Kohana_Database_Exception('database.error', $e->getMessage());
+		catch (PDOException $e) {
+			throw new DatabaseException('database error: ' . $e->getMessage());
 		}
 
 		// Clear password after successful connect
-		$this->db_config['connection']['pass'] = NULL;
+		$this->dbConfig['connection']['pass'] = NULL;
 
 		return $this->link;
 	}
 
-	public function query($sql)
-	{
-        try
-		{
+	public function query($sql) {
+        try {
 			$sth = $this->link->prepare($sql);
 		}
-		catch (PDOException $e)
-		{
-			throw new Kohana_Database_Exception('database.error', $e->getMessage());
+		catch (PDOException $e) {
+			throw new DatabaseException('database error: ' . $e->getMessage());
 		}
-		return new Pdosqlite_Result($sth, $this->link, $this->db_config['object'], $sql);
+        
+		return new PdosqliteResult($sth, $this->link, $this->dbConfig['object'],
+                                   $sql);
 	}
 
-	public function set_charset($charset)
-	{
-		$this->link->query('PRAGMA encoding = '.$this->escape_str($charset));
+	public function setCharset($charset) {
+		$this->link->query('PRAGMA encoding = '.$this->escapeStr($charset));
 	}
 
-	public function escape_table($table)
-	{
-		if ( ! $this->db_config['escape'])
+	public function escapeTable($table) {
+		if (!$this->dbConfig['escape'])
 			return $table;
 
 		return '`'.str_replace('.', '`.`', $table).'`';
 	}
 
-	public function escape_column($column)
-	{
-		if ( ! $this->db_config['escape'])
+	public function escapeColumn($column) {
+		if (!$this->dbConfig['escape'])
 			return $column;
 
 		if ($column == '*')
 			return $column;
 
 		// This matches any functions we support to SELECT.
-		if ( preg_match('/(avg|count|sum|max|min)\(\s*(.*)\s*\)(\s*as\s*(.+)?)?/i', $column, $matches))
-		{
-			if ( count($matches) == 3)
-			{
-				return $matches[1].'('.$this->escape_column($matches[2]).')';
-			}
+		if (preg_match('/(avg|count|sum|max|min)\(\s*(.*)\s*\)(\s*as\s*(.+)?)?/i',
+                       $column, $matches)) {
+			if (count($matches) == 3)
+				return $matches[1].'('.$this->escapeColumn($matches[2]).')';
 			else if ( count($matches) == 5)
-			{
-				return $matches[1].'('.$this->escape_column($matches[2]).') AS '.$this->escape_column($matches[2]);
-			}
+				return $matches[1] .'(' . $this->escapeColumn($matches[2]) .
+                    ') AS ' . $this->escapeColumn($matches[2]);
 		}
 
 		// This matches any modifiers we support to SELECT.
-		if ( ! preg_match('/\b(?:rand|all|distinct(?:row)?|high_priority|sql_(?:small_result|b(?:ig_result|uffer_result)|no_cache|ca(?:che|lc_found_rows)))\s/i', $column))
-		{
-			if (stripos($column, ' AS ') !== FALSE)
-			{
+        $regex = '/\b(?:rand|all|distinct(?:row)?|high_priority|sql_(?:' .
+            'small_result|b(?:ig_result|uffer_result)|no_cache|ca(?:che|' .
+            'lc_found_rows)))\s/i';
+		if (!preg_match($regex, $column)) {
+			if (stripos($column, ' AS ') !== false) {
 				// Force 'AS' to uppercase
 				$column = str_ireplace(' AS ', ' AS ', $column);
 
@@ -129,72 +104,57 @@ class Database_Pdosqlite_Driver extends Database_Driver {
 		$parts = explode(' ', $column);
 		$column = '';
 
-		for ($i = 0, $c = count($parts); $i < $c; $i++)
-		{
+		for ($i = 0, $c = count($parts); $i < $c; $i++) {
 			// The column is always last
 			if ($i == ($c - 1))
-			{
 				$column .= preg_replace('/[^.*]+/', '`$0`', $parts[$i]);
-			}
 			else // otherwise, it's a modifier
-			{
 				$column .= $parts[$i].' ';
-			}
 		}
 		return $column;
 	}
 
-	public function limit($limit, $offset = 0)
-	{
+	public function limit($limit, $offset = 0) {
 		return 'LIMIT '.$offset.', '.$limit;
 	}
 
-	public function compile_select($database)
-	{
+	public function compileSelect($database) {
 		$sql = ($database['distinct'] == TRUE) ? 'SELECT DISTINCT ' : 'SELECT ';
 		$sql .= (count($database['select']) > 0) ? implode(', ', $database['select']) : '*';
 
-		if (count($database['from']) > 0)
-		{
+		if (count($database['from']) > 0) {
 			$sql .= "\nFROM ";
 			$sql .= implode(', ', $database['from']);
 		}
 
-		if (count($database['join']) > 0)
-		{
-			foreach($database['join'] AS $join)
-			{
-				$sql .= "\n".$join['type'].'JOIN '.implode(', ', $join['tables']).' ON '.$join['conditions'];
-			}
+		if (count($database['join']) > 0) {
+			foreach($database['join'] AS $join) {
+				$sql .= "\n" . $join['type'] . 'JOIN ' .
+                    implode(', ', $join['tables']) . ' ON ' . $join['conditions'];
+            }
 		}
 
-		if (count($database['where']) > 0)
-		{
+		if (count($database['where']) > 0) {
 			$sql .= "\nWHERE ";
-		}
+            $sql .= implode("\n", $database['where']);
+        }
 
-		$sql .= implode("\n", $database['where']);
-
-		if (count($database['groupby']) > 0)
-		{
+		if (count($database['groupby']) > 0) {
 			$sql .= "\nGROUP BY ";
 			$sql .= implode(', ', $database['groupby']);
 		}
 
-		if (count($database['having']) > 0)
-		{
+		if (count($database['having']) > 0) {
 			$sql .= "\nHAVING ";
 			$sql .= implode("\n", $database['having']);
 		}
 
-		if (count($database['orderby']) > 0)
-		{
+		if (count($database['orderby']) > 0) {
 			$sql .= "\nORDER BY ";
 			$sql .= implode(', ', $database['orderby']);
 		}
 
-		if (is_numeric($database['limit']))
-		{
+		if (is_numeric($database['limit'])) {
 			$sql .= "\n";
 			$sql .= $this->limit($database['limit'], $database['offset']);
 		}
@@ -202,102 +162,85 @@ class Database_Pdosqlite_Driver extends Database_Driver {
 		return $sql;
 	}
 
-	public function escape_str($str)
-	{
-		if ( ! $this->db_config['escape'])
+	public function escapeStr($str) {
+		if (!$this->dbConfig['escape'])
 			return $str;
 
 		if (function_exists('sqlite_escape_string'))
-		{
 			$res = sqlite_escape_string($str);
-		}
 		else
-		{
 			$res = str_replace("'", "''", $str);
-		}
+        
 		return $res;
 	}
 
-	public function list_tables()
-	{
+	public function listTables() {
 		$sql = "SELECT `name` FROM `sqlite_master` WHERE `type`='table' ORDER BY `name`;";
-		try
-		{
+		try {
 			$result = $this->query($sql)->result(FALSE, PDO::FETCH_ASSOC);
 			$tables = array();
 			foreach ($result as $row)
-			{
 				$tables[] = current($row);
-			}
 		}
-		catch (PDOException $e)
-		{
-			throw new Kohana_Database_Exception('database.error', $e->getMessage());
+		catch (PDOException $e) {
+			throw new DatabaseException('database error: ' . $e->getMessage());
 		}
+        
 		return $tables;
 	}
 
-	public function show_error()
-	{
+	public function showError() {
 		$err = $this->link->errorInfo();
 		return isset($err[2]) ? $err[2] : 'Unknown error!';
 	}
 
-	public function list_fields($table, $query = FALSE)
-	{
+	public function listFields($table, $query = FALSE) {
 		static $tables;
-		if (is_object($query))
-		{
-			if (empty($tables[$table]))
-			{
+		if (is_object($query)) {
+			if (empty($tables[$table])) {
 				$tables[$table] = array();
 
 				foreach ($query->result() as $row)
-				{
 					$tables[$table][] = $row->name;
-				}
 			}
 
 			return $tables[$table];
 		}
-		else
-		{
-			$result = $this->link->query( 'PRAGMA table_info('.$this->escape_table($table).')' );
+		else {
+			$result = $this->link->query('PRAGMA table_info(' .
+                                         $this->escapeTable($table) .')');
 
 			foreach ($result as $row)
-			{
-				$tables[$table][$row['name']] = $this->sql_type($row['type']);
-			}
+				$tables[$table][$row['name']] = $this->sqlType($row['type']);
 
 			return $tables[$table];
 		}
 	}
 
-	public function field_data($table)
-	{
-		Kohana::log('error', 'This method is under developing');
+	public function fieldData($table) {
+		// ::log('error', 'This method is under developing');
 	}
+    
 	/**
 	 * Version number query string
 	 *
 	 * @access	public
 	 * @return	string
 	 */
-	function version()
-	{
+	function version() {
 		return $this->link->getAttribute(constant("PDO::ATTR_SERVER_VERSION"));
 	}
 
-} // End Database_PdoSqlite_Driver Class
+}
 
 /*
  * PDO-sqlite Result
  */
-class Pdosqlite_Result extends Database_Result {
+class PdosqliteResult extends DatabaseResult {
 
 	// Data fetching types
-	protected $fetch_type  = PDO::FETCH_OBJ;
-	protected $return_type = PDO::FETCH_ASSOC;
+	protected $fetchType  = PDO::FETCH_OBJ;
+	protected $returnType = PDO::FETCH_ASSOC;
 
 	/**
 	 * Sets up the result variables.
@@ -307,38 +250,34 @@ class Pdosqlite_Result extends Database_Result {
 	 * @param  boolean   return objects or arrays
 	 * @param  string    SQL query that was run
 	 */
-	public function __construct($result, $link, $object = TRUE, $sql)
-	{
-		if (is_object($result) OR $result = $link->prepare($sql))
-		{
+	public function __construct($result, $link, $object = true, $sql) {
+		if (is_object($result) || $result = $link->prepare($sql)) {
 			// run the query. Return true if success, false otherwise
-			if( ! $result->execute())
-			{
+			if(!$result->execute()) {
 				// Throw Kohana Exception with error message. See PDOStatement errorInfo() method
-				$arr_infos = $result->errorInfo();
-				throw new Kohana_Database_Exception('database.error', $arr_infos[2]);
+				$arrInfos = $result->errorInfo();
+				throw new DatabaseException('database error: ' . $arrInfos[2]);
 			}
 
-			if (preg_match('/^SELECT|PRAGMA|EXPLAIN/i', $sql))
-			{
+			if (preg_match('/^SELECT|PRAGMA|EXPLAIN/i', $sql)) {
 				$this->result = $result;
-				$this->current_row = 0;
+				$this->currentRow = 0;
 
-				$this->total_rows = $this->sqlite_row_count();
+				$this->totalRows = $this->sqliteRowCount();
 
-				$this->fetch_type = ($object === TRUE) ? PDO::FETCH_OBJ : PDO::FETCH_ASSOC;
+				$this->fetchType = ($object === TRUE) ?
+                    PDO::FETCH_OBJ : PDO::FETCH_ASSOC;
 			}
-			elseif (preg_match('/^DELETE|INSERT|UPDATE/i', $sql))
-			{
-				$this->insert_id  = $link->lastInsertId();
+			elseif (preg_match('/^DELETE|INSERT|UPDATE/i', $sql)) {
+				$this->insertId  = $link->lastInsertId();
 
-				$this->total_rows = $result->rowCount();
+				$this->totalRows = $result->rowCount();
 			}
 		}
-		else
-		{
+		else {
 			// SQL error
-			throw new Kohana_Database_Exception('database.error', $link->errorInfo().' - '.$sql);
+			throw new DatabaseException('database error: ' . $link->errorInfo() .
+                                        ' - '.$sql);
 		}
 
 		// Set result type
@@ -348,13 +287,10 @@ class Pdosqlite_Result extends Database_Result {
 		$this->sql = $sql;
 	}
 
-	private function sqlite_row_count()
-	{
+	private function sqliteRowCount() {
 		$count = 0;
 		while ($this->result->fetch())
-		{
 			$count++;
-		}
 
 		// The query must be re-fetched now.
 		$this->result->execute();
@@ -366,121 +302,100 @@ class Pdosqlite_Result extends Database_Result {
 	 * Destructor: __destruct
 	 *  Magic __destruct function, frees the result.
 	 */
-	public function __destruct()
-	{
-		if (is_object($this->result))
-		{
+	public function __destruct() {
+		if (is_object($this->result)) {
 			$this->result->closeCursor();
 			$this->result = NULL;
 		}
 	}
 
-	public function result($object = TRUE, $type = PDO::FETCH_BOTH)
-	{
-		$this->fetch_type = (bool) $object ? PDO::FETCH_OBJ : PDO::FETCH_BOTH;
+	public function result($object = TRUE, $type = PDO::FETCH_BOTH) {
+		$this->fetchType = (bool) $object ? PDO::FETCH_OBJ : PDO::FETCH_BOTH;
 
-		if ($this->fetch_type == PDO::FETCH_OBJ)
-		{
-			$this->return_type = (is_string($type) AND Kohana::auto_load($type)) ? $type : 'stdClass';
-		}
+		if ($this->fetchType == PDO::FETCH_OBJ)
+			$this->returnType = (is_string($type) && Config::autoLoad($type)) ?
+                $type : 'stdClass';
 		else
-		{
-			$this->return_type = $type;
-		}
+			$this->returnType = $type;
 
 		return $this;
 	}
 
-	public function as_array($object = NULL, $type = PDO::FETCH_ASSOC)
-	{
-		return $this->result_array($object, $type);
+	public function asArray($object = NULL, $type = PDO::FETCH_ASSOC) {
+		return $this->resultArray($object, $type);
 	}
 
-	public function result_array($object = NULL, $type = PDO::FETCH_ASSOC)
-	{
+	public function resultArray($object = NULL, $type = PDO::FETCH_ASSOC) {
 		$rows = array();
 
-		if (is_string($object))
-		{
+		if (is_string($object)) {
 			$fetch = $object;
 		}
-		elseif (is_bool($object))
-		{
-			if ($object === TRUE)
-			{
+		elseif (is_bool($object)) {
+			if ($object === true) {
 				$fetch = PDO::FETCH_OBJ;
 
 				// NOTE - The class set by $type must be defined before fetching the result,
 				// autoloading is disabled to save a lot of stupid overhead.
-				$type = (is_string($type) AND Kohana::auto_load($type)) ? $type : 'stdClass';
+				$type = (is_string($type) && Config::autoLoad($type)) ?
+                    $type : 'stdClass';
 			}
-			else
-			{
+			else {
 				$fetch = PDO::FETCH_OBJ;
 			}
 		}
-		else
-		{
+		else {
 			// Use the default config values
-			$fetch = $this->fetch_type;
+			$fetch = $this->fetchType;
 
-			if ($fetch == PDO::FETCH_OBJ)
-			{
-				$type = (is_string($type) AND Kohana::auto_load($type)) ? $type : 'stdClass';
-			}
+			if ($fetch == PDO::FETCH_OBJ) 
+				$type = (is_string($type) && Config::autoLoad($type)) ?
+                    $type : 'stdClass';
 		}
-		try
-		{
+        
+		try {
 			while ($row = $this->result->fetch($fetch))
-			{
-				$rows[] = $row;
-			}
+                $rows[] = $row;
 		}
-		catch(PDOException $e)
-		{
-			throw new Kohana_Database_Exception('database.error', $e->getMessage());
-			return FALSE;
+		catch(PDOException $e) {
+			throw new DatabaseException('database error: ' . $e->getMessage());
+			return false;
 		}
+        
 		return $rows;
 	}
 
-	public function list_fields()
-	{
-		$field_names = array();
-		for ($i = 0, $max = $this->result->columnCount(); $i < $max; $i++)
-		{
+	public function listFields() {
+		$fieldNames = array();
+		for ($i = 0, $max = $this->result->columnCount(); $i < $max; $i++) {
 			$info = $this->result->getColumnMeta($i);
-			$field_names[] = $info['name'];
+			$fieldNames[] = $info['name'];
 		}
-		return $field_names;
+        
+		return $fieldNames;
 	}
 
-	public function seek($offset)
-	{
+	public function seek($offset) {
 		// To request a scrollable cursor for your PDOStatement object, you must
 		// set the PDO::ATTR_CURSOR attribute to PDO::CURSOR_SCROLL when you
 		// prepare the statement.
-		Kohana::log('error', get_class($this).' does not support scrollable cursors, '.__FUNCTION__.' call ignored');
+		// ::log('error', get_class($this).' does not support scrollable cursors, '.__FUNCTION__.' call ignored');
 
-		return FALSE;
+		return false;
 	}
 
-	public function offsetGet($offset)
-	{
-		try
-		{
-			return $this->result->fetch($this->fetch_type, PDO::FETCH_ORI_ABS, $offset);
+	public function offsetGet($offset) {
+		try {
+			return $this->result->fetch($this->fetchType, PDO::FETCH_ORI_ABS, $offset);
 		}
-		catch(PDOException $e)
-		{
-			throw new Kohana_Database_Exception('database.error', $e->getMessage());
+		catch(PDOException $e) {
+			throw new DatabaseException('database error: ' . $e->getMessage());
 		}
 	}
 
-	public function rewind()
-	{
+	public function rewind() {
 		// Same problem that seek() has, see above.
 		return $this->seek(0);
 	}
 
-} // End PdoSqlite_Result Class
+}
