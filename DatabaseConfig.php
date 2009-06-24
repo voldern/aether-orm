93baf4472f0974d31cba6cfc7bf90f7049672993
Espen Volden <voldern@arche.hoeggen.net>
2009-06-24 16:07:22 +0200

    Added inflector stuff to config

--- DatabaseConfig.php
+++ DatabaseConfig.php
@@ -4,89 +4,89 @@ $config = array();
 
 
 $config['default'] = array(
-		'benchmark'     => true,
-		'persistent'    => false,
-		'connection'    => array(
-			'type'     => 'mysql',
-			'user'     => 'dbuser',
-			'pass'     => 'p@ssw0rd',
-			'host'     => 'localhost',
-			'port'     => false,
-			'socket'   => false,
-			'database' => 'kohana'
-		),
-		'character_set' => 'utf8',
-		'table_prefix'  => '',
-		'object'        => true,
-		'cache'         => false,
-		'escape'        => true
-);
+    'benchmark'     => true,
+    'persistent'    => false,
+    'connection'    => array(
+        'type'     => 'mysql',
+        'user'     => 'dbuser',
+        'pass'     => 'p@ssw0rd',
+        'host'     => 'localhost',
+        'port'     => false,
+        'socket'   => false,
+        'database' => 'kohana'
+        ),
+    'character_set' => 'utf8',
+    'table_prefix'  => '',
+    'object'        => true,
+    'cache'         => false,
+    'escape'        => true
+    );
 
 $config['prisguide'] = array(
-		'benchmark'     => true,
-		'persistent'    => false,
-		'connection'    => array(
-			'type'     => 'pgsql',
-			'user'     => 'prisguide',
-			'pass'     => 'j7ieaRKWaNkayD',
-			'host'     => 'dev.raw.no',
-			'port'     => false,
-			'socket'   => false,
-			'database' => 'prisguide'
-		),
-		'character_set' => 'utf8',
-		'table_prefix'  => '',
-		'object'        => true,
-		'cache'         => true,
-		'escape'        => true
-);
+    'benchmark'     => true,
+    'persistent'    => false,
+    'connection'    => array(
+        'type'     => 'pgsql',
+        'user'     => 'prisguide',
+        'pass'     => 'j7ieaRKWaNkayD',
+        'host'     => 'dev.raw.no',
+        'port'     => false,
+        'socket'   => false,
+        'database' => 'prisguide'
+        ),
+    'character_set' => 'utf8',
+    'table_prefix'  => '',
+    'object'        => true,
+    'cache'         => true,
+    'escape'        => true
+    );
 
 $config['test_mysql'] = array(
-		'benchmark'     => true,
-		'persistent'    => false,
-		'connection'    => array(
-			'type'     => 'mysql',
-			'user'     => 'testdb',
-			'pass'     => 'TYfusaK5CaPaB7fR',
-			'host'     => 'dev.raw.no',
-			'port'     => false,
-			'socket'   => false,
-			'database' => 'testdb'
-		),
-		'character_set' => 'utf8',
-		'table_prefix'  => '',
-		'object'        => true,
-		'cache'         => false,
-		'escape'        => true
-);
+    'benchmark'     => true,
+    'persistent'    => false,
+    'connection'    => array(
+        'type'     => 'mysql',
+        'user'     => 'testdb',
+        'pass'     => 'TYfusaK5CaPaB7fR',
+        'host'     => 'dev.raw.no',
+        'port'     => false,
+        'socket'   => false,
+        'database' => 'testdb'
+        ),
+    'character_set' => 'utf8',
+    'table_prefix'  => '',
+    'object'        => true,
+    'cache'         => false,
+    'escape'        => true
+    );
 
 $config['test_mysqli'] = array(
-		'benchmark'     => true,
-		'persistent'    => false,
-		'connection'    => array(
-			'type'     => 'mysqli',
-			'user'     => 'testdb',
-			'pass'     => 'TYfusaK5CaPaB7fR',
-			'host'     => 'dev.raw.no',
-			'port'     => false,
-			'socket'   => false,
-			'database' => 'testdb'
-		),
-		'character_set' => 'utf8',
-		'table_prefix'  => '',
-		'object'        => true,
-		'cache'         => false,
-		'escape'        => true
-);
+    'benchmark'     => true,
+    'persistent'    => false,
+    'connection'    => array(
+        'type'     => 'mysqli',
+        'user'     => 'testdb',
+        'pass'     => 'TYfusaK5CaPaB7fR',
+        'host'     => 'dev.raw.no',
+        'port'     => false,
+        'socket'   => false,
+        'database' => 'testdb'
+        ),
+    'character_set' => 'utf8',
+    'table_prefix'  => '',
+    'object'        => true,
+    'cache'         => false,
+    'escape'        => true
+    );
 
 $config['sql_types'] = array(
     'tinyint' => array('type' => 'int', 'max' => 127),
     'smallint' => array('type' => 'int', 'max' => 32767),
-    'mediumint'	=> array('type' => 'int', 'max' => 8388607),
+    'mediumint' => array('type' => 'int', 'max' => 8388607),
     'int' => array('type' => 'int', 'max' => 2147483647),
     'integer' => array('type' => 'int', 'max' => 2147483647),
     'bigint' => array('type' => 'int', 'max' => 9223372036854775807),
-    'float'	=> array('type' => 'float'),
+    'float' => array('type' => 'float'),
     'float unsigned' => array('type' => 'float', 'min' => 0),
     'boolean' => array('type' => 'boolean'),
     'time' => array('type' => 'string', 'format' => '00:00:00'),
@@ -136,3 +136,58 @@ $config['sql_types']['character varying'] = $config['sql_types']['varchar'];
 
 // TIME
 $config['sql_types']['time without time zone'] = $config['sql_types']['time'];
+
+$config['uncountable'] = array(
+    'access',
+    'advice',
+    'art',
+    'baggage',
+    'dances',
+    'equipment',
+    'fish',
+    'fuel',
+    'furniture',
+    'food',
+    'heat',
+    'honey',
+    'homework',
+    'impatience',
+    'information',
+    'knowledge',
+    'luggage',
+    'money',
+    'music',
+    'news',
+    'patience',
+    'progress',
+    'pollution',
+    'research',
+    'rice',
+    'sand',
+    'series',
+    'sheep',
+    'sms',
+    'species',
+    'staff',
+    'toothpaste',
+    'traffic',
+    'understanding',
+    'water',
+    'weather',
+    'work',
+    );
+
+$config['irregular'] = array(
+    'child' => 'children',
+    'clothes' => 'clothing',
+    'man' => 'men',
+    'movie' => 'movies',
+    'person' => 'people',
+    'woman' => 'women',
+    'mouse' => 'mice',
+    'goose' => 'geese',
+    'ox' => 'oxen',
+    'leaf' => 'leaves',
+    'course' => 'courses',
+    'size' => 'sizes',
+    );
