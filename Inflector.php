28a69226a5a1e5b53f16318e8d12c3e00be962b6
Espen Volden <voldern@arche.hoeggen.net>
2009-06-24 16:06:26 +0200

    Ported inflector to aether standards

--- Inflector.php
+++ Inflector.php
@@ -1,15 +1,6 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-/**
- * Inflector helper class.
- *
- * $Id: inflector.php 4072 2009-03-13 17:20:38Z jheathco $
- *
- * @package    Core
- * @author     Kohana Team
- * @copyright  (c) 2007-2008 Kohana Team
- * @license    http://kohanaphp.com/license.html
- */
-class inflector_Core {
+<?php
+
+class Inflector {
 
 	// Cached inflections
 	protected static $cache = array();
@@ -21,96 +12,88 @@ class inflector_Core {
 	/**
 	 * Checks if a word is defined as uncountable.
 	 *
-	 * @param   string   word to check
-	 * @return  boolean
+	 * @param string $str word to check
+	 * @return boolean
 	 */
-	public static function uncountable($str)
-	{
-		if (inflector::$uncountable === NULL)
-		{
+	public static function uncountable($str) {
+		if (self::$uncountable === NULL) {
 			// Cache uncountables
-			inflector::$uncountable = Kohana::config('inflector.uncountable');
+			self::$uncountable = Config::retrieve('uncountable');
 
 			// Make uncountables mirroed
-			inflector::$uncountable = array_combine(inflector::$uncountable, inflector::$uncountable);
+			self::$uncountable = array_combine(self::$uncountable,
+                                               self::$uncountable);
 		}
 
-		return isset(inflector::$uncountable[strtolower($str)]);
+		return isset(self::$uncountable[strtolower($str)]);
 	}
 
 	/**
 	 * Makes a plural word singular.
 	 *
-	 * @param   string   word to singularize
-	 * @param   integer  number of things
-	 * @return  string
+	 * @param string $str word to singularize
+	 * @param integer $count number of things
+	 * @return string
 	 */
-	public static function singular($str, $count = NULL)
-	{
+	public static function singular($str, $count = NULL) {
 		// Remove garbage
 		$str = strtolower(trim($str));
 
-		if (is_string($count))
-		{
+		if (is_string($count)) {
 			// Convert to integer when using a digit string
-			$count = (int) $count;
+			$count = (int)$count;
 		}
 
 		// Do nothing with a single count
-		if ($count === 0 OR $count > 1)
+		if ($count === 0 || $count > 1)
 			return $str;
 
 		// Cache key name
-		$key = 'singular_'.$str.$count;
+		$key = 'singular_' . $str . $count;
 
-		if (isset(inflector::$cache[$key]))
-			return inflector::$cache[$key];
+		if (isset(self::$cache[$key]))
+			return self::$cache[$key];
 
-		if (inflector::uncountable($str))
-			return inflector::$cache[$key] = $str;
+		if (self::uncountable($str))
+			return self::$cache[$key] = $str;
 
-		if (empty(inflector::$irregular))
-		{
+		if (empty(self::$irregular)) {
 			// Cache irregular words
-			inflector::$irregular = Kohana::config('inflector.irregular');
+			self::$irregular = Config::retrieve('irregular');
 		}
 
-		if ($irregular = array_search($str, inflector::$irregular))
-		{
+		if ($irregular = array_search($str, self::$irregular)) {
 			$str = $irregular;
 		}
-		elseif (preg_match('/[sxz]es$/', $str) OR preg_match('/[^aeioudgkprt]hes$/', $str))
-		{
+		elseif (preg_match('/[sxz]es$/', $str) ||
+                preg_match('/[^aeioudgkprt]hes$/', $str)) {
 			// Remove "es"
 			$str = substr($str, 0, -2);
 		}
-		elseif (preg_match('/[^aeiou]ies$/', $str))
-		{
+		elseif (preg_match('/[^aeiou]ies$/', $str)) {
 			$str = substr($str, 0, -3).'y';
 		}
-		elseif (substr($str, -1) === 's' AND substr($str, -2) !== 'ss')
-		{
+		elseif (substr($str, -1) === 's' && substr($str, -2) !== 'ss') {
 			$str = substr($str, 0, -1);
 		}
 
-		return inflector::$cache[$key] = $str;
+		return self::$cache[$key] = $str;
 	}
 
 	/**
 	 * Makes a singular word plural.
 	 *
-	 * @param   string  word to pluralize
-	 * @return  string
+	 * @param string $str word to pluralize
+     * @param int $count
+	 * @return string
 	 */
-	public static function plural($str, $count = NULL)
-	{
+	public static function plural($str, $count = NULL) {
 		// Remove garbage
 		$str = strtolower(trim($str));
 
-		if (is_string($count))
-		{
+		if (is_string($count)) {
 			// Convert to integer when using a digit string
-			$count = (int) $count;
+			$count = (int)$count;
 		}
 
 		// Do nothing with singular
@@ -118,51 +101,46 @@ class inflector_Core {
 			return $str;
 
 		// Cache key name
-		$key = 'plural_'.$str.$count;
+		$key = 'plural_' . $str . $count;
 
-		if (isset(inflector::$cache[$key]))
-			return inflector::$cache[$key];
+		if (isset(self::$cache[$key]))
+			return self::$cache[$key];
 
-		if (inflector::uncountable($str))
-			return inflector::$cache[$key] = $str;
+		if (self::uncountable($str))
+			return self::$cache[$key] = $str;
 
-		if (empty(inflector::$irregular))
-		{
+		if (empty(self::$irregular)) {
 			// Cache irregular words
-			inflector::$irregular = Kohana::config('inflector.irregular');
+			self::$irregular = Config::retrieve('irregular');
 		}
 
-		if (isset(inflector::$irregular[$str]))
-		{
-			$str = inflector::$irregular[$str];
+		if (isset(self::$irregular[$str])) {
+			$str = self::$irregular[$str];
 		}
-		elseif (preg_match('/[sxz]$/', $str) OR preg_match('/[^aeioudgkprt]h$/', $str))
-		{
+		elseif (preg_match('/[sxz]$/', $str) ||
+                preg_match('/[^aeioudgkprt]h$/', $str)) {
 			$str .= 'es';
 		}
-		elseif (preg_match('/[^aeiou]y$/', $str))
-		{
+		elseif (preg_match('/[^aeiou]y$/', $str)) {
 			// Change "y" to "ies"
 			$str = substr_replace($str, 'ies', -1);
 		}
-		else
-		{
+		else {
 			$str .= 's';
 		}
 
 		// Set the cache and return
-		return inflector::$cache[$key] = $str;
+		return self::$cache[$key] = $str;
 	}
 
 	/**
 	 * Makes a phrase camel case.
 	 *
-	 * @param   string  phrase to camelize
-	 * @return  string
+	 * @param string $str phrase to camelize
+	 * @return string
 	 */
-	public static function camelize($str)
-	{
-		$str = 'x'.strtolower(trim($str));
+	public static function camelize($str) {
+		$str = 'x' . strtolower(trim($str));
 		$str = ucwords(preg_replace('/[\s_]+/', ' ', $str));
 
 		return substr(str_replace(' ', '', $str), 1);
@@ -174,8 +152,7 @@ class inflector_Core {
 	 * @param   string  phrase to underscore
 	 * @return  string
 	 */
-	public static function underscore($str)
-	{
+	public static function underscore($str) {
 		return preg_replace('/\s+/', '_', trim($str));
 	}
 
@@ -185,8 +162,7 @@ class inflector_Core {
 	 * @param   string  phrase to make human-reable
 	 * @return  string
 	 */
-	public static function humanize($str)
-	{
+	public static function humanize($str) {
 		return preg_replace('/[_-]+/', ' ', trim($str));
 	}
 
